362799adc07f7070575a77e305b13d60424bac47
Mohammadibbu <92036859+Mohammadibbu@users.noreply.github.com>
2023-07-26 20:13:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-import firebaseConfig from "/source/firebaseinitialization.js";
+// Replace the Firebase configuration with your own project's configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCvSaKQo98k6DSzrG01bLjkOeYoeq4E7DY",
+  authDomain: "water-level-indicator-14606.firebaseapp.com",
+  projectId: "water-level-indicator-14606",
+  databaseURL:
+    "https://water-level-indicator-14606-default-rtdb.firebaseio.com/",
+  storageBucket: "water-level-indicator-14606.appspot.com",
 
+  messagingSenderId: "56987502786",
+  appId: "1:56987502786:web:fdf8ea92b2f14bdec1935b",
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
