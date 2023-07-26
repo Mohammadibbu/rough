3fc8e1b6fc7329c8971da64b3dea6f98054f8941
Mohammadibbu <92036859+Mohammadibbu@users.noreply.github.com>
2023-07-26 19:57:54 +0530

    Create firebaseinitialization.js

---
+++ source/firebaseinitialization.js
@@ -0,0 +1,14 @@
+// Replace the Firebase configuration with your own project's configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCvSaKQo98k6DSzrG01bLjkOeYoeq4E7DY",
+  authDomain: "water-level-indicator-14606.firebaseapp.com",
+  projectId: "water-level-indicator-14606",
+  databaseURL:
+    "https://water-level-indicator-14606-default-rtdb.firebaseio.com/",
+  storageBucket: "water-level-indicator-14606.appspot.com",
+
+  messagingSenderId: "56987502786",
+  appId: "1:56987502786:web:fdf8ea92b2f14bdec1935b",
+};
+
+export default firebaseConfig;
