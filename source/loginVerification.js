33b8e78bd4e97b4a7513c9883904361cfcd714ed
Mohammadibbu <92036859+Mohammadibbu@users.noreply.github.com>
2023-08-01 01:09:23 +0530

    Update loginVerification.js

--- source/loginVerification.js
+++ source/loginVerification.js
@@ -1,80 +1,73 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCvSaKQo98k6DSzrG01bLjkOeYoeq4E7DY",
-  authDomain: "water-level-indicator-14606.firebaseapp.com",
-  projectId: "water-level-indicator-14606",
-  databaseURL:
-    "https://water-level-indicator-14606-default-rtdb.firebaseio.com/",
-  storageBucket: "water-level-indicator-14606.appspot.com",
+import {
+  getAuth,
+  //database
+  connectDB,
+  ref,
+  get,
+  set,
+  child,
+  update,
+  remove,
+} from "./firebaseinitialization.js";
 
-  messagingSenderId: "56987502786",
-  appId: "1:56987502786:web:fdf8ea92b2f14bdec1935b",
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-var userAuthUid = localStorage.getItem("userid<@#(1029384756)#@>");
-var userAuthEmail = JSON.parse(
+const userAuthUid = localStorage.getItem("userid<@#(1029384756)#@>");
+const userAuthExtra = JSON.parse(
   localStorage.getItem("userEmail<@#(0192837465)#@>")
-).email;
-var userAuthEmailerified = JSON.parse(
-  localStorage.getItem("userEmail<@#(0192837465)#@>")
-).emailVerified;
-// user verification
-const db = firebase.database().ref("users");
-db.on("value", (snapshot) => {
-  var i = 1;
-  var useridverify = [];
-  snapshot.forEach((usersdata) => {
-    key = usersdata.key;
-    uservalue = usersdata.val();
-    // push values to Array
-    useridverify.push(uservalue.uid);
-    // console.log(uservalue);
-
-    i++;
-    // console.log(key)
+);
+// console.log(userAuthEmailerified.emailVerified);
+const reference = ref(connectDB);
+// get Data and Check Login Verification---------------
+function verifyUser(data) {
+  if (data) {
+    if (
+      !data.uid == userAuthUid ||
+      !data.uid == userAuthExtra.uid ||
+      !data.UserLoggedIn == true ||
+      !data.emailVerified == userAuthExtra.emailVerified
+    ) {
+      console.log("no data availBLe", data);
+      location.replace("index.html");
+    } else {
+      console.log("data avail", data);
+      document.getElementById("uname").innerText = data.email;
+    }
+  } else {
+    location.replace("index.html");
+    console.log("no data avail", data);
+  }
+}
+//-----------------Get data From Firebase database--------------
+get(child(reference, "users/" + userAuthUid))
+  .then((snapshot) => {
+    let data = snapshot.val();
+    verifyUser(data);
+  })
+  .catch((e) => {
+    console.log("error while fetching data", e);
   });
 
-  console.log(useridverify);
-  console.log(useridverify.includes(userAuthUid));
-  if (!useridverify.includes(userAuthUid)) {
-    console.log("no data avail");
+// user verification
 
-    location.replace("index.html");
-  } else {
-    userAuthEmailerified == true
-      ? firebase
-          .database()
-          .ref("users/" + userAuthUid)
-          .update({
-            emailVerified: "true",
-          })
-          .then(() => {
-            console.log("send verified");
-          })
-          .catch((e) => {
-            alert(e);
-          })
-      : console.log("email verified true");
-
-    console.log(" data avail");
-  }
-});
-
-console.log(JSON.parse(localStorage.getItem("userEmail<@#(0192837465)#@>")));
-
-// console.log(userAuth);
+// console.log(JSON.parse(localStorage.getItem("userEmail<@#(0192837465)#@>")));
 
 // ternary if else
 
 document.getElementById("logout").addEventListener("click", (e) => {
   if (confirm("Are you Sure To Logout!")) {
-    localStorage.removeItem("userid<@#(1029384756)#@>");
-    location.replace("index.html");
+    // localStorage.removeItem("userid<@#(1029384756)#@>");
+    // location.replace("index.html");
     console.log("cleared");
+    update(child(ref(connectDB), "users/" + userAuthEmailerified.uid), {
+      UserLoggedIn: false,
+    })
+      .then(() => {
+        console.log("logged out");
+      })
+      .catch((e) => {
+        alert(e);
+      });
   } else {
     // alert("you cancel the logout Process");
   }
 });
-document.getElementById("uname").innerText = userAuthEmail;
 // console.log(userAuthEmail);
