60177f2a9ff4a61c867794638326b5833e023a6a
Mohammadibbu <92036859+Mohammadibbu@users.noreply.github.com>
2023-08-03 15:01:22 +0530

    Update wliui.js

--- waterlevelUI/wliui.js
+++ waterlevelUI/wliui.js
@@ -18,25 +18,30 @@ document.addEventListener("DOMContentLoaded", function () {
   const userAuthExtra = JSON.parse(
     sessionStorage.getItem("userEmail<@#(0192837465)#@>")
   );
-  if (userAuthUid == userAuthExtra.uid) {
-    get(child(ref(connectDB), "users/" + userAuthUid))
-      .then((snapshot) => {
-        let dataL = snapshot.val();
-        productid = dataL.Regproductid;
-        console.log(dataL.Regproductid);
-        // uicheck(data.productid, data);
-        productData();
-        initialLoader.classList.add("d-none");
-      })
-      .catch((e) => {
-        console.log("error while fetching data", e);
-        // location.replace("index.html");
-      });
+
+  if (userAuthUid !== null || userAuthExtra !== null) {
+    if (userAuthUid == userAuthExtra.uid) {
+      get(child(ref(connectDB), "users/" + userAuthUid))
+        .then((snapshot) => {
+          let dataL = snapshot.val();
+          productid = dataL.Regproductid;
+          console.log(dataL.Regproductid);
+          productData();
+          initialLoader.classList.add("d-none");
+        })
+        .catch((e) => {
+          console.log("error while fetching data", e);
+          // location.replace("index.html");
+        });
+    } else {
+      console.log("no data");
+      alert("something Went Wrong...");
+      location.replace("../mainpage.html");
+      //redirect to indexpage or mainpage
+    }
   } else {
-    console.log("no data");
-    alert("something Went Wrong...");
+    alert("something Went Wrong...\nplease login again..");
     location.replace("../mainpage.html");
-    //redirect to indexpage or mainpage
   }
 
   function productData() {
@@ -70,23 +75,28 @@ toggleButton.addEventListener("click", function () {
   document.querySelector(".slider").classList.toggle("active");
   if (document.getElementById("slider").classList.contains("active")) {
     document.getElementById("auto-manual").innerHTML = "AutoMode On";
-    updatingWorkingStatus("Auto");
+    updatingWorkingmode("Auto", "disabled");
     document
       .getElementById("manual-control-btn")
       .setAttribute("disabled", "true");
+    document.getElementById("manual-status").innerHTML =
+      "Diasabled When Turn on Auto mode";
+    powerButton.classList.remove("on", isOn);
 
     //   console.log(`The class name "${classNameToCheck}" exists on the element.`);
   } else {
-    updatingWorkingStatus("manual");
+    updatingWorkingmode("manual", false);
     document.getElementById("manual-control-btn").removeAttribute("disabled");
     document.getElementById("auto-manual").innerHTML = "AutoMode Off";
+    document.getElementById("manual-status").innerHTML = "off";
 
     //   console.log(`The class name "${classNameToCheck}" does not exist on the element.` );
   }
 });
-function updatingWorkingStatus(AutoOrManual) {
+function updatingWorkingmode(AutoOrManual, manualbtnstatus) {
   update(child(ref(connectDB), "products/" + productid), {
-    Workstatus: AutoOrManual,
+    Workmode: AutoOrManual,
+    manualbutton: manualbtnstatus,
   })
     .then(() => {
       //   console.log(AutoOrManual);
@@ -95,3 +105,22 @@ function updatingWorkingStatus(AutoOrManual) {
       console.log("something Went Wrong\nplease Try Again\nERROR:", e.code);
     });
 }
+const powerButton = document.querySelector(".power-button");
+let isOn = false;
+
+powerButton.addEventListener("click", () => {
+  isOn = !isOn;
+  powerButton.classList.toggle("on", isOn);
+  if (powerButton.classList.contains("on")) {
+    document.getElementById("manual-status").innerHTML = "On";
+    updatingWorkingmode("manual", true);
+
+    //   console.log(`The class name "${classNameToCheck}" exists on the element.`);
+  } else {
+    updatingWorkingmode("manual", false);
+    document.getElementById("manual-control-btn").removeAttribute("disabled");
+    document.getElementById("manual-status").innerHTML = "Off";
+
+    //   console.log(`The class name "${classNameToCheck}" does not exist on the element.` );
+  }
+});
